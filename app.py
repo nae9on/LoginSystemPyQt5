37078049a3e86ee6427cb902cde3c2df796890ba
a.h.kadar <a.h.kadar@gmail.com>
2021-03-21 16:53:48 +0100

    MainApp

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ from contollers.main_controller import LoginSystem
 from PyQt5 import QtWidgets
 import sys
 
+
+class MainApp(QtWidgets.QApplication):
+    def __init__(self, argv):
+        super().__init__(argv)
+        self.widget = LoginSystem()
+        self.widget.show()
+
+
 if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    widget = LoginSystem()
-    widget.show()
-    sys.exit(app.exec_())
+    app = MainApp(sys.argv)
+    sys.exit(app.exec())
